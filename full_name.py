0be20b40be48c246de8ddc0d2e2eacfa1d9594b2
Saidu Yauba <programmabletailor@gmail.com>
2025-10-24 20:33:10 +0100

    Add files via upload

---
+++ full_name.py
@@ -0,0 +1,7 @@
+first_name = 'saidu'
+second_name = 'yauba'
+
+#full_name = first_name + second_name
+full_name = f"{first_name} {second_name}"
+message = f" Hello, {full_name.title()}"
+print(message)
