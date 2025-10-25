19e28a541f515e1c8f74ed907a038e24c9468ffa
Saidu Yauba <programmabletailor@gmail.com>
2025-10-25 15:11:46 +0100

    Upload names list

---
+++ names.py
@@ -0,0 +1,11 @@
+names = ['umar','abbas','tanaga','sultan']
+#print(names[0].title())
+#print(names[1].title())
+#print(names[2].title())
+#print(names[3].title())
+#using loop
+for name in names:
+    print(name.title())
+# to print message with personal name
+for name in names:
+    print( f"Hello Mr {name.title()}, How are you")
