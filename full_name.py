28e51c8630c5ac7bae9005541c2b0baf906fc316
Saidu Yauba <programmabletailor@gmail.com>
2025-10-25 15:09:52 +0100

    Update greeting message in full_name.py
    Fix message formatting to include 'how are you'.

--- full_name.py
+++ full_name.py
@@ -3,5 +3,5 @@ second_name = 'yauba'
 
 #full_name = first_name + second_name
 full_name = f"{first_name} {second_name}"
-message = f" Hello, {full_name.title()}"
+message = f"Hello, {full_name.title()how are you}"
 print(message)
